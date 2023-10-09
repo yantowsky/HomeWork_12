258817b604a731462039ae17cfa01c2ff223e454
yantowsky <it.yantowsky@gmail.com>
2023-10-09 23:48:32 +0300

    Added a solution for the first task

---
+++ main.js
@@ -0,0 +1,8 @@
+let firtsWord = prompt("Введіть перше слово:");
+let secondWord = prompt("Введіть друге слово:");
+let thirdWord = prompt("Введіть третьє слово:");
+
+let allWords = [thirdWord, firtsWord, secondWord].join(" ");
+
+console.log("Перше завдання:");
+console.log(allWords);
